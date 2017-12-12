044679ebe034f6a9e12386a7cdfa2683b797925e
Imran Syed <syed.imran2512@gmail.com>
2017-12-12 16:16:34 -0500

    part done

--- app.js
+++ app.js
@@ -6,8 +6,34 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 
+const authRoutes = require('./routes/auth-routes');
+const mapRoutes = require('./routes/map-routes');
+const session    = require('express-session');
+const passport   = require('passport');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+require('dotenv').load();
+mongoose.connect(process.env.MONGODB_URI);
+
+const passportSetup = require('./config/passport');
+
 
 const app = express();
+
+var whitelist = [
+    'http://localhost:4200',
+];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +50,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+passportSetup(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const index = require('./routes/index');
 app.use('/', index);
+
+app.use('/', authRoutes);
+app.use('/', mapRoutes);
+
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

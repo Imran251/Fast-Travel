e62a0b9b5246caea0461df7b972a981922bc2934
Imran Syed <syed.imran2512@gmail.com>
2017-12-12 21:33:12 -0500

    done models

---
+++ models/user-places.js
@@ -0,0 +1,16 @@
+/* jshint esversion:6 */
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const placesSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: String,
+  location: String,
+  icon: String
+});
+
+module.exports = mongoose.model('Place', placesSchema);
---
+++ models/user-routes.js
@@ -0,0 +1,25 @@
+/* jshint esversion:6 */
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const routesSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  place: String,
+  time: String,
+  type: String,
+  mapBD: [
+    {
+      icon: 'string',
+      name: 'string',
+      vicinity: 'string'
+    }
+  ],
+  comment: [String]
+});
+
+
+module.exports = mongoose.model('Route', routesSchema);
